a2a751f303907f470dcecf7a2117df2c7f0a775b
MontyMango <59061157+MontyMango@users.noreply.github.com>
2022-08-04 16:47:48 -0400

    Counting! | v0.26
    The script can now count to see how many computers are online and how many are offline! 🔢

--- ping_script.sh
+++ ping_script.sh
@@ -3,37 +3,44 @@
 # Looks in the Hosts.txt
 HOST_LIST=/home/localadmin/Documents/Hosts.txt
 NOT_FOUND=/tmp/$$
+FOUND=/tmp/$$1
 DATE=$(date +%R-%m-%d-%Y)
 RESULTS=Scan_Test_$DATE.txt
 
-touch $RESULTS
+F_NUM=0
+NF_NUM=0
 
+touch $RESULTS
+touch $FOUND
 
 $(echo date) > $RESULTS
-echo "" >> $RESULTS
-echo "" >> $RESULTS
-echo "Found:" >> $RESULTS
 
 for i in $(cat $HOST_LIST)
 do
 	# Variable searches in the host list line by line for a running computer
 	HOST=$(host $i | grep "has address")
-	echo $i
+	echo "Pinging $i..."
 	
 	# If the host is empty
 	if [ -z "$HOST" ]
 	then
 		echo $i >> $NOT_FOUND
+		let "NF_NUM += 1"
 	
 	# If the host is running
 	else
-		echo -e "$i \t|\t $HOST" >> $RESULTS
+		echo -e "$i \t|\t$HOST" >> $FOUND
+		let "F_NUM += 1"
 	fi
+	
 done
 
-
+echo "" >> $RESULTS
+echo "" >> $RESULTS
+echo -e "Found\t\t|\tTotal $F_NUM:" >> $RESULTS
+cat $FOUND >> $RESULTS
 
 echo "" >> $RESULTS
 echo "" >> $RESULTS
-echo "Not Found:" >> $RESULTS
+echo -e "Not Found | Total $NF_NUM:" >> $RESULTS
 cat $NOT_FOUND >> $RESULTS
