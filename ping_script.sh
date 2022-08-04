a3e7f164b19c41a5bc0c8d87979d86cbf1959471
MontyMango <59061157+MontyMango@users.noreply.github.com>
2022-08-04 16:44:53 -0400

    Big changes | v0.25
    Features:
    + A new file will be created with the results when you start the script up, instead of overriding it.
    + The list now contains the hosts that have not been found when the script executes.

--- ping_script.sh
+++ ping_script.sh
@@ -1,12 +1,39 @@
 #!/bin/bash
 
 # Looks in the Hosts.txt
-LIST=/home/localadmin/Documents/Hosts.txt
+HOST_LIST=/home/localadmin/Documents/Hosts.txt
+NOT_FOUND=/tmp/$$
+DATE=$(date +%R-%m-%d-%Y)
+RESULTS=Scan_Test_$DATE.txt
 
-$(echo date) > Hosts_Found.txt
+touch $RESULTS
 
-for i in $(cat $LIST)
+
+$(echo date) > $RESULTS
+echo "" >> $RESULTS
+echo "" >> $RESULTS
+echo "Found:" >> $RESULTS
+
+for i in $(cat $HOST_LIST)
 do
+	# Variable searches in the host list line by line for a running computer
+	HOST=$(host $i | grep "has address")
 	echo $i
-	echo $(host $i) | grep "has address" >> Hosts_Found.txt
+	
+	# If the host is empty
+	if [ -z "$HOST" ]
+	then
+		echo $i >> $NOT_FOUND
+	
+	# If the host is running
+	else
+		echo "$i | $HOST" >> $RESULTS
+	fi
 done
+
+
+
+echo "" >> $RESULTS
+echo "" >> $RESULTS
+echo "Not Found:" >> $RESULTS
+cat $NOT_FOUND >> $RESULTS
