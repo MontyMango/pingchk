71b7e4dd8622e688fc7d9babf681ff92bfa94469
MontyMango <59061157+MontyMango@users.noreply.github.com>
2022-09-14 23:33:40 -0400

    Start up added
    Explains what is going on with the program, this also makes the results directory, and makes the bash files executable.

---
+++ start_up.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo Welcome to pingchk!
+echo This application is currently in beta so stuff might not work and might change!
+echo
+echo To begin, fill out the hosts that you want to ping in the Hosts.txt file.
+echo And if you want to automate this, be sure to use \"crontab -e\" to automate!
+echo
+echo For further information refer to the README.md. Thank you!
+
+$(bash ./scripts/mk_results_dir.sh)
+chmod a+x ping_script.sh
+$(chmod a+x ./scripts/*.sh)
